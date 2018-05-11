dcd1c4fc079ae9d4e64c7311a188fc440ed7ab68
IrvinT <irvinthomas@outlook.fr>
2018-05-11 10:40:54 +0200

    Enhance charts (#21)
    * Add failure jobs count on last 7 days as a stats redis key

---
+++ src/Charts/Charts.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the AllProgrammic Resque package.
+ *
+ * (c) AllProgrammic SAS <contact@allprogrammic.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace AllProgrammic\Component\Resque\Charts;
+
+use AllProgrammic\Component\Resque\Worker;
+
+/**
+ * Class Charts
+ *
+ * @package AllProgrammic\Component\Resque\Charts\Processed
+ */
+class Charts
+{
+    const INDEX_FAILURE = 'charts:failure';
+    const INDEX_PROCESS = 'charts:process';
+
+    /**
+     * Redis constructor.
+     *
+     * @param \AllProgrammic\Component\Resque\Redis $backend
+     */
+    public function __construct(\AllProgrammic\Component\Resque\Redis $backend)
+    {
+        $this->backend = $backend;
+    }
+
+    public function getFailure()
+    {
+        return new \AllProgrammic\Component\Resque\Charts\Failure\Redis($this->backend);
+    }
+
+    public function getProcess()
+    {
+        return new \AllProgrammic\Component\Resque\Charts\Process\Redis($this->backend);
+    }
+}
